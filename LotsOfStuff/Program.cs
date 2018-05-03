50c4cc88cf472ac93c3defe3a0c18b3064e3c92a
Rodr1go Garcia <gemeosgarcia@gmail.com>
2018-05-03 12:56:09 +0100

    Adição de uma Gun a bag do player e correção do bug que estava a ocurrer

--- LotsOfStuff/Program.cs
+++ LotsOfStuff/Program.cs
@@ -35,6 +35,8 @@ namespace Aula10
             p.BagOfStuff.AddThing(new Gun(1.5f, 0.050f, 10, 250));
             // 200 gramas de fruta fresca
             p.BagOfStuff.AddThing(new Food(FoodType.Fruit, 0, 0.200f));
+            // Pistola com 1.5kg + 50 gramas por bala, carregada com 10 balas, com um custo de 250€
+            p.BagOfStuff.AddThing(new Gun(1.5f, 0.050f, 10, 250));
 
             // Quantos itens tem o jogador na mochila?
             Console.WriteLine($"Nº de itens na mochila: {p.BagOfStuff.StuffCount}");
